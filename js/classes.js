970cc8e5fc304ce2796e24ff6e05ef3781844d36
TheBreadfish <108020725+TheBreadfish@users.noreply.github.com>
2022-08-12 20:33:49 -0700

    Create classes.js
    Classes for creating shops

---
+++ js/classes.js
@@ -0,0 +1,8 @@
+class Shop {
+    constructor(name, description, datecreated, owner) {
+        this.name = name;
+        this.description = description;
+        this.datecreated = datecreated;
+        this.owner = owner;
+    }
+}
