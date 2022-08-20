ae02d2df7e14f6c582fded31b00765c52f36f7df
TheBreadfish <108020725+TheBreadfish@users.noreply.github.com>
2022-08-20 09:02:03 -0700

    added script for shops

--- js/shops.js
+++ js/shops.js
@@ -1,4 +1,13 @@
-  let Shops = [];
+/*  USEFULL PARTS
+
+ Line 35: Where you paste the functions to add new shops (newShop())
+ Line 70: Where the shops are displayed
+ Line 106: genCode() function
+ Line 116: Code handling displaying the shop
+
+    - if you want to know how/what to put in the shopGen() function check classes.js (js/classes.js)    */
+
+let Shops = [];
 
 function newShop(name, description, owner, datecreated, keywords, loc, items, eta, stock, meta) {
     let newShop = new Shop(name, description, datecreated, owner, JSON.parse(keywords), loc, JSON.parse(items), eta, stock, meta);
@@ -51,7 +60,7 @@ newShop("name", "description", "owner", insertCommas("1660846734070"), '["keywor
 
 newShop("name", "description", "owner", insertCommas("1660846734070"), '["keyword1", "keyword2"]', "location", '["item1","item2"]', "{timeMin:'1', timeMax:'2', unit:'unit'}", "stock", "meta")
 
-newShop("name", "description", "owner", insertCommas("1660846734070"), '["keyword1", "keyword2"]', "location", '["item1","item2"]', "{timeMin:'1', timeMax:'2', unit:'unit'}", "stock", "meta")
+newShop("shop name this is a shop name yes hello hi", "description", "owner", insertCommas("1660602333316"), '["keyword1", "keyword2"]', "location", '["item1","item2"]', "{timeMin:'1', timeMax:'2', unit:'unit'}", "stock", "meta");
 
 /*
 Do NOT paste the new shop function below this comment
@@ -108,6 +117,9 @@ function shopOpen(i) {
   console.log(i)
 
   let shopContent = `
+  <br>
+  <a onClick="window.location.href=window.location.href">back to shops</a>
+  <br>
   <k>`+Shops[i].name+`</k>
   <br>
   <span style="font-size: x-large">`+Shops[i].owner+`</span>
@@ -120,3 +132,32 @@ function shopOpen(i) {
 
   content.innerHTML = "<br><br><br><br>" + shopContent
 }
+
+
+/*
+https://www.w3schools.com/howto/howto_js_search_menu.asp
+*/
+function search() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("searchBar");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("shops");
+  li = ul.getElementsByTagName("li");
+
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerHTML;
+
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
+
+let searchIdeas = [
+  "Games", "3d Prints", "Candy", "Food", "how to cure depression", "Art", "Drinks"
+]
+
+document.getElementById("searchBar").placeholder = 'Search for "'+searchIdeas[Math.floor(Math.random() * searchIdeas.length)]+'"'
